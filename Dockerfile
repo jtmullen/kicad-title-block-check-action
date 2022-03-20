983191b2d027294526193fed2a2879aca4c4c9e5
jtmullen <jt11mullen@gmail.com>
2022-03-20 12:11:33 -0500

    Rewrite for v6, cleanup, first test

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,9 @@ RUN apk add python3
 RUN apk add py3-pip
 RUN pip3 install gitpython
 RUN pip3 install PyYAML
-RUN git clone https://github.com/realthunder/kicad_parser.git \
+RUN git clone https://github.com/jtmullen/kicad_parser.git \
 && cd kicad_parser \
-&& git checkout 5ec854b3119071b1b56ad534900f495a91ba6a09 \
+&& git checkout fc8b0a56d70e0772f6f10915cbb4b4557b98d9a5 \
 && git submodule update --init
 
 CMD ["/main.py"]
--- main.py
+++ main.py
@@ -14,10 +14,18 @@ checkSCH = False
 failed = ""
 fields = ['pageSize','title','company','rev','date','comment1','comment2','comment3','comment4']
 
+
+## There was an unrecoverable error running the action
+## Immediately Exit
 def error(cause):
 	print("::error::{}".format(cause))
 	exit(1)
 	
+
+## Add a failed file to the output
+## Print the file name and fail reason
+## If a file fails multiple times will print new case
+##     but will not add duplicated to failed
 def fail(fileName, cause):
 	global failed
 	print("::error file={}::{}".format(fileName, cause))
@@ -25,14 +33,57 @@ def fail(fileName, cause):
 		failed += fileName
 		failed += ","
 
+
+##  Check Title Blocks in a S Expression File
+##  Used for PCBs in v5&6 and Schematics in v6
+##  They are mostly the same
+def checkSExpBlock(checks, commentRegex, sexp, file):
+	if "pageSize" in checks:
+		## For some reason page size is "page" in .kicad_pcb and "paper" in .kicad_sch
+		if "page" in sexp or "paper" in sexp:
+			if "page" in sexp:
+				size = sexp.page
+			else:
+				size = sexp.paper
+			if not pcb_checks["pageSize"] in size:
+				fail(file, "Expected Page Size {}, found {}".format(checks["pageSize"], size))
+		else:
+			fail(file, "Page size not found")
+
+	## Check the non-comment fields
+	for field in ["title", "rev", "company", "date"]:
+		if field in checks:
+			if field in sexp.title_block:
+				if not re.match(checks[field], sexp.title_block[field].strip("\"")):
+					fail(file, "{}: \"{}\", does not match \"{}\"".format(field, sexp.title_block[field], checks[field])) 
+			else:
+				fail(file, "{} not found, expected match: {}".format(field, checks[field]))
+
+	## Check the comments
+	comments = ["", "", "", ""]
+	if "comment" in sexp.title_block:
+		sexpComments = sexp.title_block.comment
+		if isinstance(sexpComments, list):  ## If it is a list there is only 1 comment
+			comments[sexpComments[0]-1] = sexpComments[1]
+		else:
+			for item in sexpComments:
+				comments[item[0]-1] = item[1]
+				
+	for i in range(0,len(comments)):
+		if not re.match(commentRegex[i], comments[i].strip("\"")):
+			fail(file, "Comment {}: \"{}\", does not match \"{}\"".format(i+1, comments[i], commentRegex[i]))
+
+
+## Main function to run checks
 def main():
 	print("::group::Set Up")
 	print("Python Version: {}".format(sys.version))
 
 	try:
 		from kicad_parser import KicadPCB
+		from kicad_parser import KicadSCH
 	except ImportError:
-		error("Error importing KiCad PCB Dependency")
+		error("Error importing KiCad Parser Dependency")
 
 	try:
 		eventStream = open(os.environ["GITHUB_EVENT_PATH"], 'r')
@@ -128,7 +179,8 @@ def main():
 		print(sch_checks)
 
 	pcbsToCheck = []
-	schToCheck = []
+	schToCheck_v5 = []
+    schToCheck_v6 = []
 
 	if os.environ["INPUT_CHECK_ALL"] != "false":
 		print("Checking all files in Repo")
@@ -136,8 +188,10 @@ def main():
 		for file in allFiles:
 			if checkPCB and file.name.endswith(".kicad_pcb"):
 				pcbsToCheck.append(str(file))
+			elif checkSCH and file.name.endswith(".kicad_sch"):
+				schToCheck_v6.append(str(file))
 			elif checkSCH and file.name.endswith(".sch"):
-				schToCheck.append(str(file))
+				schToCheck_v5.append(str(file))
 	else:
 		print("Checking Changed Files")
 		format = '--name-only'
@@ -153,8 +207,10 @@ def main():
 		for file in allFiles:
 			if checkPCB and file.endswith(".kicad_pcb"):
 				pcbsToCheck.append(file)
+			elif checkSCH and file.name.endswith(".kicad_sch"):
+				schToCheck_v6.append(file)
 			elif checkSCH and file.endswith(".sch"):
-				schToCheck.append(file)
+				schToCheck_v5.append(file)
 
 
 	print("::endgroup::")
@@ -167,119 +223,127 @@ def main():
 		for field in pcb_checks:
 			if field not in fields:
 				print("::warning file={}::Unknown PCB Field: {}".format(regexFile, field))
-
+		
+		## Turn the comments into something more useful
 		pcbCommentRegex = []
 		pcbCommentRegex.append("(.*)" if not "comment1" in pcb_checks else pcb_checks["comment1"])
 		pcbCommentRegex.append("(.*)" if not "comment2" in pcb_checks else pcb_checks["comment2"])
 		pcbCommentRegex.append("(.*)" if not "comment3" in pcb_checks else pcb_checks["comment3"])
 		pcbCommentRegex.append("(.*)" if not "comment4" in pcb_checks else pcb_checks["comment4"])
 		for file in pcbsToCheck:
-			print("Checking PCB: {}".format(file))
-
+			print("::group::{}".format(file))
 			pcb = KicadPCB.load(file)
-
 			pcbError = False
 			for e in pcb.getError():
+				print("::endgroup::")
 				fail(file, "{}".format(e))
 				pcbError = True
 			
 			if not pcbError:
 				# Check Page
-				if "pageSize" in pcb_checks:
-					if "page" in pcb:
-						page = pcb.page
-						if not pcb_checks["pageSize"] in page:
-							fail(file, "Expected Page Size {}, found {}".format(pcb_checks["pageSize"], page))
-					else:
-						fail(file, "Page size not found")
-				
 				if "title_block" in pcb:
-					for field in ["title", "rev", "company", "date"]:
-						if field in pcb_checks:
-							if field in pcb.title_block:
-								if not re.match(pcb_checks[field], pcb.title_block[field].strip("\"")):
-									fail(file, "{}: \"{}\", does not match \"{}\"".format(field, pcb.title_block[field], pcb_checks[field])) 
-							else:
-								fail(file, "{} not found, expected match: {}".format(field, pcb_checks[field]))
-				
-					comments = ["", "", "", ""]
-					
-					if "comment" in pcb.title_block:
-						pcbComments = pcb.title_block.comment
-						if isinstance(pcbComments, list):
-							comments[pcbComments[0]-1] = pcbComments[1]
-						else:
-							for item in pcbComments:
-								comments[item[0]-1] = item[1]
-								
-					for i in range(0,4):
-						if not re.match(pcbCommentRegex[i], comments[i].strip("\"")):
-							fail(file, "Comment {}: \"{}\", does not match \"{}\"".format(i+1, comments[i], pcbCommentRegex[i]))
+					checkSExpBlock(pcb_checks, pcbCommentRegex, pcb, file)
+					print("::endgroup::")
 				else:
+					print("::endgroup::")
 					fail(file, "Title Block Not Found")
 		print("::endgroup::")
 		
-	if not schToCheck:
+	if not (schToCheck_v5 or schToCheck_v6):
 		print("No Schematics to Check")
 	else:
 		print("::group::Schematic Checks")
 		
-		schFieldMaps = {'title':'Title',
-						'company':'Comp',
-						'rev':'Rev',
-						'date':'Date',
-						'comment1':'Comment1',
-						'comment2':'Comment2',
-						'comment3':'Comment3',
-						'comment4':'Comment4'}
+		for field in pcb_checks:
+			if field not in fields:
+				print("::warning file={}::Unknown PCB Field: {}".format(regexFile, field))
+				
+		if schToCheck_v6:
+			print("::group::{}".format(file))
 
-		schExpectedFields = {}
-		for field in fields:
-			schExpectedFields[field] = False
+			## Turn the comments into something more useful
+			schCommentRegex = []
+			schCommentRegex.append("(.*)" if not "comment1" in sch_checks else sch_checks["comment1"])
+			schCommentRegex.append("(.*)" if not "comment2" in sch_checks else sch_checks["comment2"])
+			schCommentRegex.append("(.*)" if not "comment3" in sch_checks else sch_checks["comment3"])
+			schCommentRegex.append("(.*)" if not "comment4" in sch_checks else sch_checks["comment4"])
+			for file in pcbsToCheck:
+				print("::group::{}".format(file))
+				sch = KicadSCH.load(file)
+				schError = False
+				for e in sch.getError():
+					print("::endgroup::")
+					fail(file, "{}".format(e))
+					schError = True
+				
+				if not schError:
+					# Check Page
+					if "title_block" in sch:
+						checkSExpBlock(sch_checks, schCommentRegex, sch, file)
+						print("::endgroup::")
+					else:
+						print("::endgroup::")
+						fail(file, "Title Block Not Found")
+		
+		if schToCheck_v5:
+			schFieldMaps = {'title':'Title',
+							'company':'Comp',
+							'rev':'Rev',
+							'date':'Date',
+							'comment1':'Comment1',
+							'comment2':'Comment2',
+							'comment3':'Comment3',
+							'comment4':'Comment4'}
 
-		for field in sch_checks:
-			if field in fields:
-				schExpectedFields[field] = True
-			else:
-				print("::warning file={}::Unknown Schematic Field: {}".format(regexFile, field))
+			schExpectedFields = {}
+			for field in fields:
+				schExpectedFields[field] = False
 
-		for file in schToCheck:
-			print("Checking Schematic: {}".format(file))
+			for field in sch_checks:
+				if field in fields:
+					schExpectedFields[field] = True
+				else:
+					print("::warning file={}::Unknown Schematic Field: {}".format(regexFile, field))
 
-			thisCheck = schExpectedFields
-			descrFound = False
-			
-			try:
-				f = open(file, 'r')
-			except OSError:
-				fail(file, "Error Opening File")
-				break;
-			lines = f.readlines()
-			for line in lines:
-				if "$Descr" in line:
-					descrFound = True
-					if thisCheck['pageSize']:
-						if not sch_checks["pageSize"] in line:
-							fail(file, "Expected page size: {}, found: {}".format(sch_checks["pageSize"], line))
-					thisCheck['pageSize'] = False
-				elif "$EndDescr" in line:
-					break
-				elif descrFound:
-					for field in thisCheck:
-						if thisCheck[field]:
-							if re.match("{} \"(.*)\"".format(schFieldMaps[field]), line):
-								match = re.match("{} \"(.*)\"".format(schFieldMaps[field]), line)
-								if not re.match(sch_checks[field], match.group(1)):
-									fail(file, "{}: \"{}\", does not match: \"{}\"".format(field, match.group(1), sch_checks[field]))
-								thisCheck[field] = False
-								break
-							
-			# Make sure we found all we were looking for
-			for field in schExpectedFields:
-				if schExpectedFields[field]:
-					fail(file, "Field {} Not Found in Schematic".format(field))
-			
-			f.close()
+			for file in schToCheck_v5:
+				print("::group::{}".format(file))
+
+				thisCheck = schExpectedFields
+				descrFound = False
+				
+				try:
+					f = open(file, 'r')
+				except OSError:
+					fail(file, "Error Opening File")
+					break;
+				lines = f.readlines()
+				for line in lines:
+					if "$Descr" in line:
+						descrFound = True
+						if thisCheck['pageSize']:
+							if not sch_checks["pageSize"] in line:
+								fail(file, "Expected page size: {}, found: {}".format(sch_checks["pageSize"], line))
+						thisCheck['pageSize'] = False
+					elif "$EndDescr" in line:
+						break
+					elif descrFound:
+						for field in thisCheck:
+							if thisCheck[field]:
+								if re.match("{} \"(.*)\"".format(schFieldMaps[field]), line):
+									match = re.match("{} \"(.*)\"".format(schFieldMaps[field]), line)
+									if not re.match(sch_checks[field], match.group(1)):
+										fail(file, "{}: \"{}\", does not match: \"{}\"".format(field, match.group(1), sch_checks[field]))
+									thisCheck[field] = False
+									break
+								
+				# Make sure we found all we were looking for
+				for field in schExpectedFields:
+					if schExpectedFields[field]:
+						fail(file, "Field {} Not Found in Schematic".format(field))
+				
+				f.close()
+				print("::endgroup::")
+		
 		print("::endgroup::")
 
 	print("::set-output name=fails::{}".format(failed))
